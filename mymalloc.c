494d2a1e6016312dc41b581d96a987f0b335b50e
edmond <edma_wu@yahoo.com>
2016-09-27 15:29:15 -0400

    added int variable to track amount of free space

--- mymalloc.c
+++ mymalloc.c
@@ -4,36 +4,36 @@
 #include "mymalloc.h"
 
 static char heap[5000];
+int free_space = 5000;
 
 void *mymalloc(size_t size) {
-	if (size > 5000) {
+	if (size > free_space)
 		fprintf(stderr, "Not enough space\n");
-	}
 	else {
 		int s = size;
 		int first_open = 0;
 		for (int i = 0; i < 5000; i++) {
 			if (heap[i] == 'a') {
 				first_open = i;
+				allocate(first_open, s);
 			}
-			allocate(first_open, s);
 		}
 		printf("Size: %d\n", s);
 	}
 }
 
 void myfree(void *ptr) {
-	if (ptr == NULL) {
+	if (ptr == NULL)
 		fprintf(stderr, "Invalid free\n");
-	}
 }
 
 void allocate(int start, int size) {
-	if (size + start > 5000) {
+	if (size + start > 5000)
 		fprintf(stderr, "Not enough space\n");
-	}
-	for (int i = start; i < size + start; i++) {
-		heap[i] = 'b';
+	else {
+		for (int i = start; i < size + start; i++)
+			heap[i] = 'b';
+		free_space -= size;
 	}
 }
 
@@ -43,14 +43,12 @@ char* get_heap() {
 
 void initialize_heap() {
 	//a for unused, b for used
-	for (int i = 0; i < 5000; i++) {
+	for (int i = 0; i < 5000; i++)
 		heap[i] = 'a';
-	}
 }
 
 void print_heap() {
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 10; i++)
 		printf("%c ", heap[i]);
-	}
 	printf("\n");
 }
