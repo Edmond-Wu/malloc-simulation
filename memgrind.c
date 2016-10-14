a1d00ce3ed0121bd120a2b96977339c9c13da88d
edmond <edma_wu@yahoo.com>
2016-10-13 22:42:14 -0400

    workload

--- memgrind.c
+++ memgrind.c
@@ -9,9 +9,21 @@
 int main(int argc, char *argv[]) {
 	//test case A
 	double average_a = 0.0, average_b = 0.0, average_c = 0.0, average_d = 0.0, average_e = 0.0, average_f = 0.0;
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 100; i++) {
 		average_a += workload_a();
+		average_b += workload_b();
+		average_c += workload_c();
+		average_d += workload_d();
+		average_e += workload_e();
+		average_f += workload_f();
+	}
 	average_a /= 100;
+	average_b /= 100;
+	average_c /= 100;
+	average_d /= 100;
+	average_e /= 100;
+	average_f /= 100;
+
 	printf("Average time for A: %f\n", average_a);
 
   	return 0;
